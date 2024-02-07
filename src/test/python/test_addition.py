1929ea58a30273fca4234bc1770d07ddfcf7eb50
Deswaerte Florent <pro.flo.contact@gmail.com>
2024-02-07 10:46:25 +0100

    Ajout maven et test

--- src/test/python/test_addition.py
+++ src/test/python/test_addition.py
@@ -1,6 +1,8 @@
-def test_addition():
-    assert 5 + 5 == 10, "Erreur: 5 + 5 n'est pas égal à 10"
-    print("[TEST-Addition] -> Le test d'addition a reussi !")
+import unittest
+
+class TestAddition(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(5 + 5, 10, "Erreur: 5 + 5 n'est pas égal à 10")
 
 if __name__ == "__main__":
-    test_addition()
+    unittest.main()
