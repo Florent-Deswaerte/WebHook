4cc42567e0ae3474ae70dbc7d4f5158178078a0b
Deswaerte Florent <pro.flo.contact@gmail.com>
2024-02-07 09:51:46 +0100

    Ajout maven et test

---
+++ src/test/python/test_addition.py
@@ -0,0 +1,6 @@
+def test_addition():
+    assert 5 + 5 == 10, "Erreur: 5 + 5 n'est pas égal à 10"
+    print("[TEST-Addition] -> Le test d'addition a reussi !")
+
+if __name__ == "__main__":
+    test_addition()
